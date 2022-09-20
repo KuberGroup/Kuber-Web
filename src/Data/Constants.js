24d031b1768451d4ae1b08609a72393470d4f323
Mohammad Danish <mohddanish8299@gmail.com>
2022-09-20 15:40:47 +0530

    added constants data

---
+++ src/Data/Constants.js
@@ -0,0 +1,5 @@
+export const TITLE = 'Kuber'
+export const FULL_TITLE = 'Kuber Group'
+export const MINI_DESCRIPTION = 'Online Messaging Platform'
+export const LOGIN = 'Log In'
+export const SIGNUP = 'Sign Up'
