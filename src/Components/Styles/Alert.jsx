97bf7dbd799a5225a002d58bb9e14ca04d266cc1
Mohammad Danish <mohddanish8299@gmail.com>
2022-09-22 19:47:31 +0530

    created custom alert component

---
+++ src/Components/Styles/Alert.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Alert.css";
+import { TbAlertCircle } from "react-icons/tb";
+
+const AlertMsg = ({ text }) => {
+  return (
+    <div className="Alert">
+      <TbAlertCircle
+        color="#ef5350"
+        style={{
+          marginRight: "2px",
+          padding: "7px 0",
+          display: "flex",
+          fontSize: 32,
+          opacity: "0.9",
+        }}
+      />
+      <div>{text}</div>
+    </div>
+  );
+};
+
+export default AlertMsg;
