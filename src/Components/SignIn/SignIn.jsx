a1ede367ab826dc504b59f5521fb62fb20131a54
Mohd Danish <mohddanish8299@gmail.com>
2022-09-22 13:24:46 +0530

    removed log

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -43,10 +43,7 @@ export default function SignIn() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+
     try {
       setError("");
       setLoading(true);
