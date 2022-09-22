ad30b50961903969826ad1fa134813807bfff523
Mohammad Danish <mohddanish8299@gmail.com>
2022-09-22 19:47:58 +0530

    used floating input and alert in signup

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,10 +1,12 @@
 import React, { useRef, useState } from "react";
-import { Form, Button, Card, Alert } from "react-bootstrap";
+import { Form, Button, Card, FloatingLabel } from "react-bootstrap";
 import { useAuth } from "../../Context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import { MINI_DESCRIPTION, TITLE, FORM } from "../../Data/Constants";
 import { useTitle } from "../../Hooks/useTitle";
 import LoginContainer from "../Containers/LoginContainer";
+import "./style.css";
+import AlertMsg from "../Styles/Alert";
 
 const SignUp = () => {
   const emailRef = useRef();
@@ -39,34 +41,52 @@ const SignUp = () => {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">{FORM.signup.title}</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
+          {error && <AlertMsg text={error} />}
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" id="email">
-              <Form.Label>{FORM.label.email.title}</Form.Label>
-              <Form.Control
-                type="email"
-                ref={emailRef}
-                placeholder={FORM.label.email.placeholder}
-                required
-              />
+              <FloatingLabel
+                controlId="floatingInput"
+                label={FORM.label.email.title}
+                className="mb-3"
+              >
+                <Form.Control
+                  type="email"
+                  placeholder={FORM.label.email.placeholder}
+                  ref={emailRef}
+                  required
+                  className="FormInput"
+                />
+              </FloatingLabel>
             </Form.Group>
             <Form.Group className="mb-3" id="password">
-              <Form.Label>{FORM.label.password.title}</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordRef}
-                placeholder={FORM.label.password.placeholder}
-                required
-              />
+              <FloatingLabel
+                controlId="floatingInput"
+                label={FORM.label.password.title}
+                className="mb-3"
+              >
+                <Form.Control
+                  type="password"
+                  placeholder={FORM.label.password.placeholder}
+                  ref={passwordRef}
+                  required
+                  className="FormInput"
+                />
+              </FloatingLabel>
             </Form.Group>
             <Form.Group className="mb-3" id="passwordConfirm">
-              <Form.Label>{FORM.label.confPassword.title}</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordConfirmRef}
-                placeholder={FORM.label.confPassword.placeholder}
-                required
-              />
+              <FloatingLabel
+                controlId="floatingInput"
+                label={FORM.label.confPassword.title}
+                className="mb-3"
+              >
+                <Form.Control
+                  type="password"
+                  placeholder={FORM.label.confPassword.placeholder}
+                  ref={passwordConfirmRef}
+                  required
+                  className="FormInput"
+                />
+              </FloatingLabel>
             </Form.Group>
             <Button disabled={loading} className="w-100" type="submit">
               {FORM.signup.title}
@@ -75,8 +95,9 @@ const SignUp = () => {
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        {FORM.haveAccount.title}{" "}
-        <Link to={FORM.haveAccount.url}>{FORM.haveAccount.button}</Link>
+        <Link to={FORM.haveAccount.url}>
+          {FORM.haveAccount.title} {FORM.haveAccount.button}
+        </Link>
       </div>
     </LoginContainer>
   );
