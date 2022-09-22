f316427d4427ddc0477312cd0ab0bf00d0be7f6f
Mohammad Danish <mohddanish8299@gmail.com>
2022-09-22 20:20:28 +0530

    changed width of login container

--- src/Components/Containers/LoginContainer.jsx
+++ src/Components/Containers/LoginContainer.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import CenterContainer from './CenterContainer'
+import React from "react";
+import CenterContainer from "./CenterContainer";
 
 const LoginContainer = ({ children }) => {
-    return (
-        <CenterContainer
-            style={{ minHeight: '100vh' }}>
-            <div className='w-100' style={{ maxWidth: '400px' }}>{children}</div>
-        </CenterContainer>
-    )
-}
+  return (
+    <CenterContainer style={{ minHeight: "100vh" }}>
+      <div className="w-100" style={{ maxWidth: "420px" }}>
+        {children}
+      </div>
+    </CenterContainer>
+  );
+};
 
-export default LoginContainer
+export default LoginContainer;
