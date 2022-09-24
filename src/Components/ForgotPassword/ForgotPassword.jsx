4cdf9c1fe3d3e5c9152e027634dd4263c2d922d3
Mohd Danish <mohddanish8299@gmail.com>
2022-09-24 11:51:52 +0530

    rewrite forgot password form using native components

--- src/Components/ForgotPassword/ForgotPassword.jsx
+++ src/Components/ForgotPassword/ForgotPassword.jsx
@@ -1,14 +1,16 @@
-import React, { useRef, useState } from "react";
-import { Form, Button, Card } from "react-bootstrap";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../Context/AuthContext";
 import { MINI_DESCRIPTION, TITLE, FORM } from "../../Data/Constants";
 import { useTitle } from "../../Hooks/useTitle";
 import LoginContainer from "../Containers/LoginContainer";
+import AuthHeader from "../Headers/AuthHeader";
 import AlertMsg from "../Styles/Alert";
+import { FormButton } from "../Styles/Button";
+import FormInput from "../Styles/Input";
 
 const ForgotPassword = () => {
-  const emailRef = useRef();
+  const [email, setEmail] = useState("");
   const { resetPassword } = useAuth();
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
@@ -23,45 +25,37 @@ const ForgotPassword = () => {
       setMessage("");
       setError("");
       setLoading(true);
-      await resetPassword(emailRef.current.value);
-      setMessage(`${FORM.error.reset.success} ${emailRef.current.value}`);
+      await resetPassword(email);
+      setMessage(`${FORM.error.reset.success} ${email}`);
     } catch (e) {
-      setError(`${FORM.error.reset.failed} ${e.message}`);
+      setError(`${FORM.error.reset.failed} ${e.code}`);
     }
     setLoading(false);
   };
 
   return (
     <LoginContainer>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">{FORM.recovery.title}</h2>
-          {error && <AlertMsg variant="danger" text={error} />}
-          {message && <AlertMsg variant="success" text={message} />}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" id="email">
-              <Form.Label>{FORM.label.email.title}</Form.Label>
-              <Form.Control
-                type="email"
-                ref={emailRef}
-                placeholder={FORM.label.password.placeholder}
-                required
-              />
-            </Form.Group>
+      <AuthHeader>{FORM.recovery.title}</AuthHeader>
+      {error && <AlertMsg variant="danger" text={error} />}
+      {message && <AlertMsg variant="success" text={message} />}
+      <form onSubmit={handleSubmit}>
+        <div className="mb-1" id="email">
+          <FormInput
+            handleInputData={setEmail}
+            value={email}
+            type="email"
+            label={FORM.label.email.title}
+            required
+          />
+        </div>
 
-            <Button disabled={loading} className="w-100" type="submit">
-              {FORM.recovery.button}
-            </Button>
-          </Form>
-          <div className="w-100 text-center mt-3">
-            {FORM.haveAccount.knowPassword}{" "}
-            <Link to={FORM.haveAccount.url}>{FORM.haveAccount.button}</Link>
-          </div>
-        </Card.Body>
-      </Card>
+        <FormButton disabled={loading} className="w-100" type="submit">
+          {FORM.recovery.button}
+        </FormButton>
+      </form>
       <div className="w-100 text-center mt-2">
-        {FORM.noAccount.title}{" "}
-        <Link to={FORM.noAccount.url}>{FORM.noAccount.button}</Link>
+        <Link to={FORM.haveAccount.url}>
+          {FORM.haveAccount.knowPassword}{" "}{FORM.haveAccount.button}</Link>
       </div>
     </LoginContainer>
   );
