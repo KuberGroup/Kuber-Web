0c654423839602615453c1d1ef2a8942a7b7f957
Mohd Danish <mohddanish8299@gmail.com>
2022-09-24 11:15:30 +0530

    created copyright component

---
+++ src/Components/Copyright/Copyright.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { FULL_TITLE } from '../../Data/Constants';
+
+const Copyright = () => {
+    return (
+        <p style={{
+            margin: 0,
+            fontFamily: 'Roboto, Helvetica, Arial,sans-serif',
+            fontWeight: 400,
+            fontSize: '0.875rem',
+            lineHeight: 1.43,
+            letterSpacing: '0.01071em',
+            textAlign: 'center',
+            color: 'rgba(0, 0, 0, 0.6)',
+        }}
+        >
+            {"Copyright © "}
+            <Link style={{ color: 'inherit' }} href="https://github.com/KuberGroup" >
+                {FULL_TITLE}
+            </Link> {" "}
+            {new Date().getFullYear()}
+            {"."}
+        </p>
+    );
+
+}
+
+export default Copyright
