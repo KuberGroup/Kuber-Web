56fd6c75bb8659ea60af368ca87c75110c784140
Mohammad Danish <mohddanish8299@gmail.com>
2022-09-28 19:18:15 +0530

    fixed buttons

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import Ripple from '../Ripple/Ripple'
-import './Button.scss'
-import { BiMessageSquareAdd } from 'react-icons/bi'
+import React from "react";
+import Ripple from "../Ripple/Ripple";
+import "./Button.scss";
+import { BiMessageSquareAdd } from "react-icons/bi";
 
 export const Button = (props) => {
-    const { children } = props
-    return (
-        <button {...props}>
-            {children}
-            < Ripple />
-        </button >
-    )
-}
+  const { children } = props;
+  return (
+    <button {...props}>
+      {children}
+      <Ripple />
+    </button>
+  );
+};
 
 export const FormButton = (props) => {
-    const { children, className } = props
-    return <Button {...props} className={`Button FormButton ${className}`}>{children}</Button>
-}
+  const { children, className } = props;
+  return (
+    <Button {...props} className={`Button FormButton ${className}`}>
+      {children}
+    </Button>
+  );
+};
 
 export const StartNewChatButton = (props) => {
-    return (
-        <div className='p-abs b-0 r-0 fl fl-c m-1 c-p StartNewChatButton' {...props}>
-            <BiMessageSquareAdd color='#fff' />
-        </div>
-    )
-}
+  return (
+    <Button
+      className="p-abs b-0 r-0 fl fl-c m-1 c-p StartNewChatButton"
+      {...props}
+    >
+      <BiMessageSquareAdd color="#fff" size={24} className="icon" />
+    </Button>
+  );
+};
