3731b5fe51196c913b6542b6874c85a2fcdd16ba
Mohammad Danish <mohddanish8299@gmail.com>
2022-09-28 18:10:03 +0530

    create basic layout

--- src/Pages/StartNewChat/StartNewChat.jsx
+++ src/Pages/StartNewChat/StartNewChat.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { MainContainer } from "../../Components";
 
 const StartNewChat = () => {
-    return (
-        <div>StartNewChat</div>
-    )
-}
+  return (
+    <MainContainer>
+      <div className="p-rel w-100 h-100vh" style={{ maxWidth: 480 }}>
+        <div
+          className=" p-rel fl fl-c fl-d-col w-100 h-100 m-0"
+          style={{
+            maxWidth: 480,
+            marginTop: "45px",
+          }}
+        >
+          StartNewChat
+        </div>
+      </div>
+    </MainContainer>
+  );
+};
 
-export default StartNewChat
+export default StartNewChat;
