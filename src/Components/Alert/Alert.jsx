4467f61625675a1b85d8b38fcf58a4c1f3fc6114
Mohammad Danish <mohddanish8299@gmail.com>
2022-09-29 21:13:47 +0530

    redefined alert msg to show msg with variant [error, success]

--- src/Components/Alert/Alert.jsx
+++ src/Components/Alert/Alert.jsx
@@ -1,21 +1,16 @@
 import React from "react";
-import "./Alert.css";
-import { TbAlertCircle } from "react-icons/tb";
+import "./Alert.scss";
+import { TbAlertCircle, TbCircleCheck } from "react-icons/tb";
 
-export const AlertMsg = ({ text, className, style }) => {
+export const AlertMsg = ({ className, style, variant = "error", children }) => {
   return (
-    <div className={`Alert ${className}`} style={style}>
-      <TbAlertCircle
-        color="#ef5350"
-        className="fl"
-        style={{
-          marginRight: "2px",
-          padding: "7px 0",
-          fontSize: 32,
-          opacity: "0.9",
-        }}
-      />
-      <div>{text}</div>
-    </div >
+    <div className={`Alert ${className} ${variant}`} style={style}>
+      {variant === "success" ? (
+        <TbCircleCheck color="#0f5132" className="fl icon" />
+      ) : (
+        <TbAlertCircle color="#ef5350" className="fl icon" />
+      )}
+      <div>{children}</div>
+    </div>
   );
 };
