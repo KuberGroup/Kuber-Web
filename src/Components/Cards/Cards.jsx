4a2391f391d40f84cb686da929cddccc28594e47
Mohammad Danish <mohddanish8299@gmail.com>
2022-09-29 18:51:20 +0530

    updated user card to show basic user data [needs more data/update]

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React from "react";
 
-export const UserCard = () => {
-    return (
-        <div className='fl' style={{ background: '#fff', padding: '.5rem 1rem' }}>
-            <div className='user-pic c-p' style={{ width: 50, height: 50, borderRadius: '50%', background: '#eee' }}></div>
-            <div className='user-data fl pl-1 c-p' style={{ width: 'calc(100% - 50px)' }}>
-                <div className='name-data w-100 fl fl-d-col fl-j-se lhinit h-100 ellipsis'>
-                    <div className='name-item ellipsis'>Username</div>
-                    <div className='message-item ellipsis' style={{ fontSize: 14, color: '#ccc' }}>hello this is message</div>
-                </div>
-                <div className='timestamp fl fl-c' style={{ width: 80, fontSize: 10, color: '#ccc' }}>10:03 AM</div>
-            </div>
+export const UserCard = ({ user, newChat = false }) => {
+  if (!user)
+    user = {
+      displayname: "Loading...",
+      email: "Loading...",
+      photoURL: "Loading...",
+      uid: null,
+    };
+
+  return (
+    <div className="fl" style={{ background: "#fff", padding: ".5rem 1rem" }}>
+      <div
+        className="user-pic c-p"
+        style={{
+          width: 50,
+          height: 50,
+          borderRadius: "50%",
+          background: "#eee",
+        }}
+      ></div>
+      <div
+        className="user-data fl pl-1 c-p"
+        style={{ width: "calc(100% - 50px)" }}
+      >
+        <div className="name-data w-100 fl fl-d-col fl-j-se lhinit h-100 ellipsis">
+          <div className="name-item ellipsis">{user.displayName}</div>
+          <div
+            className="message-item ellipsis"
+            style={{ fontSize: 14, color: "#ccc" }}
+          >
+            {newChat
+              ? `Click to Start new chat with ${user.displayName}`
+              : "Last message"}
+          </div>
         </div>
-    )
-}
+        <div
+          className="timestamp fl fl-c"
+          style={{ width: 80, fontSize: 10, color: "#ccc" }}
+        >
+          {newChat ? "" : "2:30 PM"}
+        </div>
+      </div>
+    </div>
+  );
+};
