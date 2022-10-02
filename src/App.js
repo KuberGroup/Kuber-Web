886b2a2a9a10509b940526189be7f18e105e36c2
Mohammad Danish <mohddanish8299@gmail.com>
2022-10-02 16:57:10 +0530

    used chatProvider

--- src/App.js
+++ src/App.js
@@ -4,26 +4,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { RequireAuth } from './Components/';
 import { Home, SignIn, SignUp, VerifyEmail, ForgotPassword, CreateProfile, StartNewChat } from './Pages';
 import './Helpers/Spacing.scss'
+import { ChatProvider } from './Context/ChatContext';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <AuthProvider>
-          <Routes>
+          <ChatProvider>
+            <Routes>
+              {/* Routes which require Login */}
+              <Route exact path='/' element={<RequireAuth><Home /></RequireAuth>} />
+              <Route exact path='/create-profile' element={<RequireAuth><CreateProfile /></RequireAuth>} />
+              <Route exact path='/start-new-chat' element={<RequireAuth><StartNewChat /></RequireAuth>} />
+              <Route path='/verify-email' element={<RequireAuth><VerifyEmail /></RequireAuth>} />
+              {/* Routes which require Login */}
 
-            {/* Routes which require Login */}
-            <Route exact path='/' element={<RequireAuth><Home /></RequireAuth>} />
-            <Route exact path='/create-profile' element={<RequireAuth><CreateProfile /></RequireAuth>} />
-            <Route exact path='/start-new-chat' element={<RequireAuth><StartNewChat /></RequireAuth>} />
-            <Route path='/verify-email' element={<RequireAuth><VerifyEmail /></RequireAuth>} />
-            {/* Routes which require Login */}
-
-
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/login' element={<SignIn />} />
-            <Route path='/forgot-password' element={<ForgotPassword />} />
-          </Routes>
+              <Route path='/signup' element={<SignUp />} />
+              <Route path='/login' element={<SignIn />} />
+              <Route path='/forgot-password' element={<ForgotPassword />} />
+            </Routes>
+          </ChatProvider>
         </AuthProvider>
       </Router>
     </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,65 +1,18 @@
-import { collection, doc, getDoc, onSnapshot, orderBy, query, where } from 'firebase/firestore'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Copyright, FormInput, LeftMessage, MainContainer, RightMessage, UserCard } from '../../Components'
 import { StartNewChatButton } from '../../Components/Button/Button'
-import { useAuth } from '../../Context/AuthContext'
-import { db } from '../../firebase'
 import './Home.scss'
 import { BiMessageSquareAdd } from 'react-icons/bi'
+import { useChat } from '../../Context/ChatContext'
+
 const Home = () => {
+    const { chats, isEmpty, loading } = useChat()
     const navigate = useNavigate()
-    const { currentUser } = useAuth()
-    const [chats, setChats] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [isEmpty, setIsEmpty] = useState(true)
 
     const handleStartNewChat = () => {
         navigate('start-new-chat')
     }
-
-    useEffect(() => {
-        const chatListQuery = query(
-            collection(db, 'chatRoom'),
-            where('members', 'array-contains', currentUser.uid),
-            where('group', '==', false),
-            orderBy('recentMessage.sendAt', 'desc')
-        )
-
-        const unsubscribe = onSnapshot(chatListQuery, (querySnapshot) => {
-            if (querySnapshot.empty) {
-                setIsEmpty(true)
-                setLoading(false)
-            }
-            else {
-                setIsEmpty(false)
-                setLoading(true)
-                const queryPromises = querySnapshot.docs.map((item) => {
-                    return new Promise((resolve, reject) => {
-                        const temp = [];
-                        const freindId = item.data().members.filter((member) => member !== currentUser.uid);
-                        getDoc(doc(db, 'users', freindId[0])).then((snapshot) => {
-                            temp.push(snapshot.data().displayName);
-                            temp.push(snapshot.data().photoURL);
-                            resolve({
-                                ...item.data(), id: item.id, displayName: temp[0], photoURL: temp[1],
-                            })
-                        })
-                    })
-                })
-                Promise
-                    .all(queryPromises)
-                    .then(chatsData => {
-                        setChats(chatsData)
-                    })
-                    .finally(() => {
-                        setLoading(false)
-                    })
-            }
-        });
-
-        return () => unsubscribe()
-    }, [currentUser.uid, isEmpty])
 
     if (loading) {
         return <MainContainer logout={true}><div className='fl-c' style={{ fontSize: 32 }}>Loading...</div></MainContainer>
