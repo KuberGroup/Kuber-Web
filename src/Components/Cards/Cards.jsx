633db73a24d8dc3eda1e1a5fc16f798271e96133
Mohammad Danish <mohddanish8299@gmail.com>
2022-10-31 19:20:00 +0530

    fixed/modified user-card

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -20,53 +20,20 @@ export const UserCard = (props) => {
 
   return (
     <div className={`fl user ${className}`}>
-      <div
-        className="p-rel fl c-p w-100"
-        style={{
-          padding: ".5rem 1rem",
-          overflow: "hidden",
-        }}
-        {...rest}
-      >
+      <div className="p-rel fl c-p w-100" {...rest}>
         {user.photoURL ? (
-          <img
-            src={user.photoURL}
-            alt=""
-            className="user-pic c-p"
-            style={{
-              width: 50,
-              height: 50,
-              borderRadius: "50%",
-              background: "#eee",
-              border: "none",
-            }}
-          />
+          <img src={user.photoURL} alt="" className="user-pic c-p" />
         ) : (
-          <div
-            className="user-pic c-p fl fl-c"
-            style={{
-              width: 50,
-              height: 50,
-              borderRadius: "50%",
-              background: "#eee",
-              border: "none",
-            }}
-          >
+          <div className="user-pic c-p fl fl-c">
             <BiUser size={35} />
           </div>
         )}
 
-        <div
-          className="user-data fl pl-1 c-p"
-          style={{ width: "calc(100% - 50px)" }}
-        >
-          <div className="name-data w-100 fl fl-d-col fl-j-se lhinit h-100 ellipsis">
+        <div className="user-data fl fl-d-col pl-1 c-p">
+          <div className="name-data fl lhinit ellipsis">
             <div className="fl fl-j-sb">
               <div className="name-item ellipsis">{user.displayName}</div>
-              <div
-                className="timestamp fl fl-c pr-1"
-                style={{ fontSize: 10, color: "#ccc" }}
-              >
+              <div className="timestamp fl fl-c">
                 {newChat
                   ? ""
                   : user.recentMessage.sendAt
@@ -78,31 +45,19 @@ export const UserCard = (props) => {
                   : "s"}
               </div>
             </div>
-            <div className="fl fl-j-sb">
-              <div
-                className="message-item ellipsis"
-                style={{ fontSize: 14, color: "#ccc" }}
-              >
-                {newChat
-                  ? `Click to Start new chat with ${user.displayName}`
-                  : user.recentMessage.messageText}
-              </div>
-              {user.unseenMessageCount?.[currentUser.uid] > 0 && (
-                <div className="timestamp fl fl-c pr-1">
-                  <span
-                    style={{
-                      fontSize: 10,
-                      color: "#ccc",
-                      background: "#6e00ff",
-                      padding: ".2rem .3rem",
-                      borderRadius: "25px",
-                    }}
-                  >
-                    {user.unseenMessageCount?.[currentUser.uid]}
-                  </span>
-                </div>
-              )}
+          </div>
+
+          <div className="message-data fl fl-j-sb lhinit">
+            <div className="message-item ellipsis">
+              {newChat
+                ? `Click to Start new chat with ${user.displayName}`
+                : user.recentMessage.messageText}
             </div>
+            {user.unseenMessageCount?.[currentUser.uid] > 0 && (
+              <div className="notif-badge fl fl-c">
+                <span>{user.unseenMessageCount?.[currentUser.uid]}</span>
+              </div>
+            )}
           </div>
         </div>
         <Ripple />
